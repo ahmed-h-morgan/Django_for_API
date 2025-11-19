f3d8154bff9ba89a2e769570194f147a3cc1156d
ahmed-h-morgan <ahmedmorgan210@gmail.com>
2025-11-19 11:52:08 +0000

    add tests for books app

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView    # new
+
+from .models import Book
+
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
