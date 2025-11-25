4ee65299c2bd86cae990c20a251c13f994119236
ahmed-h-morgan <ahmedmorgan210@gmail.com>
2025-11-25 09:14:18 +0000

    configure the new customized User model to use for authentication instead of the default one

--- blogapi/accounts/models.py
+++ blogapi/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser           # the AbstractUser is the model to use for customizing users
+
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100)
--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "accounts.CustomUser"            # new - use the new customized User model for authentication rather than the default User model
+
+
+
+     
